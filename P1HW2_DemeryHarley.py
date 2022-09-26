ee4c231498f269da7f63eb12a308fd18d8043461
Demeryh7011 <113921427+Demeryh7011@users.noreply.github.com>
2022-09-26 15:44:29 -0400

    Add files via upload

---
+++ P1HW2_DemeryHarley.py
@@ -0,0 +1,25 @@
+Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/harle/AppData/Local/Programs/Python/Python310/P1HW2_DemeryHarley.py
+This program calculates and displays travel expenses
+
+Enter Budget: 1200
+
+Enter your travel destination: Asheville
+
+How much do you think you will spend on gas? 250
+
+Approximately, how much will you need for accomodation/hotel? 300
+
+Last, how much do you need for food? 200
+
+------------Travel Expenses------------
+Location: Asheville
+Initial Budget: 1200.0
+
+Fuel: 250.0
+Accomodation: 300.0
+Food 200.0
+
+Remaining balance: 450.0
