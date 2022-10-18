3a523ec26f3e1da30e017ea38fbd92b3c6e5a915
Demeryh7011 <113921427+Demeryh7011@users.noreply.github.com>
2022-10-17 21:32:17 -0400

    Add files via upload

---
+++ P2HW2_DemeryHarley.py
@@ -0,0 +1,17 @@
+Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/harle/AppData/Local/Programs/Python/Python310/P2HW2_DemeryHarley.py
+Enter grade for module 1 : 65.5
+Enter grade for module 2 : 88
+Enter grade for module 3 : 78.5
+Enter grade for module 4 : 90
+Enter grade for module 5 : 61
+Enter grade for module 6 : 92
+
+-----------Results------------
+Lowest grade  :    61.0
+Highest grade :    92.0
+Sum of grades :    475.0
+Average       :    79.17
+--------------------------------
